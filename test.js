dad71c11e1335cf36400454d9df09878cf0e8fc0
Dennis Gurock <dg@testmo.com>
2023-02-02 09:50:10 +0100

    Initial commit

---
+++ test.js
@@ -0,0 +1,40 @@
+const chai = require('chai');
+const assert = chai.assert;
+
+describe('files', function () {
+    describe('export', function () {
+        it('should export pdf', function () {
+            assert.isTrue(true);
+        });
+
+        it('should export html', function () {
+            assert.isTrue(true);
+        });
+
+        it('should export yml', function () {
+            assert.isTrue(true);
+        });
+
+        it('should export text', function () {
+            assert.isTrue(true);
+        });
+    });
+
+    describe('import', function () {
+        it('should import pdf', function () {
+            assert.isTrue(true);
+        });
+
+        it('should import html', function () {
+            assert.isTrue(true);
+        });
+
+        it('should import yml', function () {
+            assert.isTrue(true);
+        });
+
+        it('should import text', function () {
+            assert.isTrue(true);
+        });
+    });
+});
